bf37bbce6a9e71d417c0f4e6660be677efee015e
Lalocadelosmichis <isidora.pizarro96@gmail.com>
2023-07-16 22:56:18 -0400

    proyecto remuneraciones electrolitos

---
+++ Empleado.cs
@@ -0,0 +1,99 @@
+﻿namespace RemuneracionesElectrolitos
+{
+    // Enumeracion TipoEmpleado
+    public enum TipoEmpleado
+    {
+        JefeTienda = 0,
+        VendedorTienda = 1,
+        ReponedorTienda = 2
+    }
+    public class Empleado
+    {
+        // Atributos
+        private TipoEmpleado _empleado;
+        private string _nombre;
+        private const int _horasMaximo = 40;
+        private int _horas;
+        private double[] _sueldo;
+        private double[] _horasExtra;
+
+        // Constructores
+        public Empleado()
+        {
+            this._empleado = TipoEmpleado.VendedorTienda;
+            this._nombre = string.Empty;
+            this._horas = _horasMaximo;
+            this._sueldo = new double[] { 2000.0, 1500.0, 1000.0 };
+            this._horasExtra = new double[] { 3000.0, 2500.0, 2000.0 };
+        }
+        public Empleado(int horas, TipoEmpleado tipoEmpleado, string nombre) : this()
+        {
+            Horas = horas;
+            EmpleadoTipo = tipoEmpleado;
+            Nombre = nombre;
+        }
+        [22:26]
+        // Encapsulamiento
+        public TipoEmpleado EmpleadoTipo
+        {
+            get { return this._empleado; }
+            set { this._empleado = value; }
+        }
+        public string Nombre
+        {
+            get { return this._nombre; }
+            set { this._nombre = value; }
+        }
+        public int Horas
+        {
+            get { return this._horas; }
+            set { this._horas = value; }
+        }
+        public int HorasExtra
+        {
+            get
+            {
+                int resultado = this._horas - _horasMaximo;
+                return resultado < 0 ? 0 : resultado;
+            }
+        }
+
+        public double MontoHorasNormales
+        {
+            get
+            {
+                return _sueldo[(int)_empleado];
+
+            }
+
+        }
+
+        public double MontoHorasExtras
+        {
+            get
+            {
+
+                return _horasExtra[(int)_empleado];
+            }
+        }
+
+        public int HorasMaximo
+        {
+            get
+            {
+                return _horasMaximo;
+            }
+        }
+        public double Sueldo
+        {
+            get
+            {
+                if (Math.Abs(_horas) <= _horasMaximo)
+                    return _sueldo[(int)_empleado] * _horas;
+                else
+
+                    return (_sueldo[(int)_empleado] * _horasMaximo) + (HorasExtra * _horasExtra[(int)_empleado]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿namespace RemuneracionesElectrolitos
+{
+    public class Program
+    {
+        private static List<Empleado> empleados = new List<Empleado>(10);
+
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Elija una opcion:\n");
+                Console.WriteLine("1. Consultar usuario.");
+                Console.WriteLine("2. Ingresar usuario.\n");
+                Console.WriteLine("0. Salir\n");
+                int peticion = 0;
+                int.TryParse(Console.ReadLine(), out peticion);
+                Console.Clear();
+                switch (peticion)
+                {
+                    case 0:
+                        // Salir
+                        Console.WriteLine("Cerrando programa..");
+                        return;
+                        [22:27]
+case 1:
+                        // Consulta de datos
+                        foreach (Empleado empleado in empleados)
+                        {
+                            Console.WriteLine("__");
+                            Console.WriteLine("Nombre: " + empleado.Nombre);
+                            Console.WriteLine("Tipo empleado: " + empleado.EmpleadoTipo.ToString());
+                            Console.WriteLine("Sueldo: " + empleado.Sueldo);
+                            Console.WriteLine("Horas trabajadas: " + empleado.Horas);
+                            Console.WriteLine("Horas extra: " + empleado.HorasExtra);
+                            if (empleado.Horas <= empleado.HorasMaximo)
+                            {
+                                Console.WriteLine("Monto horas normales: " + empleado.Horas * empleado.MontoHorasNormales);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Monto horas normales: " + empleado.HorasMaximo * empleado.MontoHorasNormales);
+                            }
+
+                            Console.WriteLine("Monto horas extras: " + empleado.HorasExtra * empleado.MontoHorasExtras);
+                            Console.WriteLine("__");
+                        }
+                        break;
+                        [22:27]
+case 2:
+                        // Ingreso de datos
+
+                        Console.WriteLine("Ingrese nombre de usuario: ");
+                        string name;
+                        name = Console.ReadLine();
+                        Console.WriteLine("\nIngrese el tipo de empleado: ");
+                        Console.WriteLine("\n0: Jefe de Tienda" +
+                                          "\n1: Vendedor de Tienda" +
+                                          "\n2: Reponedor de Tienda");
+                        TipoEmpleado emp = new TipoEmpleado();
+                        int tipe = 0;
+                        int.TryParse(Console.ReadLine(), out tipe);
+                        switch (tipe)
+                        {
+                            case 0:
+                                emp = TipoEmpleado.JefeTienda;
+                                break;
+                            case 1:
+                                emp = TipoEmpleado.VendedorTienda;
+                                break;
+                            case 2:
+                                emp = TipoEmpleado.ReponedorTienda;
+                                break;
+
+                        }
+                        Console.WriteLine("\nIngrese la cantidad de horas: ");
+                        int h = 0;
+                        int.TryParse(Console.ReadLine(), out h);
+
+                        empleados.Add(new Empleado(h, emp, name));
+                        break;
+
+
+                }
+
+            }
+        }
+    }
+}
+
+Enviar mensaje a #trabajosvergas
